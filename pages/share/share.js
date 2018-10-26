a3b3666cfd90504aff394a1fabd449a2c5c3cecd
gloryliu <unknown@example.com>
2018-10-26 17:11:06 +0800

    fx..

--- pages/share/share.js
+++ pages/share/share.js
@@ -2,11 +2,28 @@
 var app = getApp();
 Page({
   data: {
-    openid: ''
+    id: '',
+    code: 0
   },
   onLoad: function (res) {
-    this.setData({
-      openid: res.openid
+    if (Object.keys(res).length!=0){
+      this.setData({
+        id: res.id
+      })
+    }
+  },
+  zhuli: function(){
+    var openid = wx.getStorageSync('openId') 
+    wx.request({
+      url: 'https://www.yuebaoyuan.com.cn/wx/public/index.php/apii/getPartakeNum',
+      method: 'POST',
+      data: {
+        'id': this.data.id,
+        'openid': openid
+      },
+      success: function (data) {
+
+      }
     })
   }
 })
